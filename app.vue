0dca3172e1e5a864f88806212b33a57f9a7d089e
yudax42 <yudax42@gmail.com>
2023-07-22 21:03:48 +0100

    feat: add vue notion plugin

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NotionRenderer :blockMap="blockMap" fullPage />
 </template>
+
+<script lang="ts" setup>
+const { $notion } = useNuxtApp();
+
+// use Notion module to get Notion blocks from the API via a Notion pageId
+const { data: blockMap } = useAsyncData("page_nuxt", () =>
+  $notion.getPageBlocks("147d16c50e5a4c2da2aa1cf88ac2a55c")
+);
+</script>
+
+<style>
+@import "vue-notion/src/styles.css";
+</style>
